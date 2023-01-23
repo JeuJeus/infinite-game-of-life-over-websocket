ebae14871fb19210350e7ee641980fce677fc8e4
Julius Figge <julius@figge.eu>
2023-01-23 14:02:10 +0100

    introduce bundling and serving via jetty webserver

--- backend/src/main/java/de/jeujeus/game/of/life/WebServer.java
+++ backend/src/main/java/de/jeujeus/game/of/life/WebServer.java
@@ -1,30 +1,51 @@
 package de.jeujeus.game.of.life;
 
 import de.jeujeus.game.of.life.websocket.ConnectionEndpoint;
+import lombok.SneakyThrows;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.DefaultHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.websocket.javax.server.config.JavaxWebSocketServletContainerInitializer;
+
+import java.net.URL;
 
 
 public class WebServer {
     private final Server server;
     private final ServerConnector connector;
 
+    @SneakyThrows
     public WebServer() {
         server = new Server();
         connector = new ServerConnector(server);
         server.addConnector(connector);
 
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
+        ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        contextHandler.setContextPath("/");
+        contextHandler.setInitParameter("org.eclipse.jetty.servlet.Default.dirAllowed", "false");
 
         JavaxWebSocketServletContainerInitializer
-                .configure(context, (servletContext, wsContainer) -> {
+                .configure(contextHandler, (servletContext, wsContainer) -> {
             wsContainer.setDefaultMaxTextMessageBufferSize(65535);
             wsContainer.addEndpoint(ConnectionEndpoint.class);
         });
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        URL webAppDir =
+                Main.class.getClassLoader().getResource("META-INF/resources");
+        resourceHandler.setResourceBase(webAppDir.toURI().toString());
+        resourceHandler.setWelcomeFiles(new String[]{ "index.html" });
+        resourceHandler.setDirectoriesListed(false);
+
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] { resourceHandler, contextHandler, new DefaultHandler() });
+        server.setHandler(handlers);
+
     }
 
     public void setPort(int port) {
