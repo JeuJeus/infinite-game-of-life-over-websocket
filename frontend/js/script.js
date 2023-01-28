b422078f3c68db696f4d724e6718e7221a0b8fa0
Julius Figge <julius.figge@klosebrothers.de>
2023-01-28 09:53:12 +0100

    WIP

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -7,32 +7,38 @@ const AmountOfCellsOnYAxis = 90;
 const pixelSize = 10;
 
 let currentGeneration = 0;
+let field;
 
 let canvasClicked = false;
+let canvasOffsetX, canvasOffsetY;
 let lastX = 0;
 let lastY = 0;
 
 const makeCursor = cursor => document.body.style.cursor = cursor;
 
 const fitCanvasSize = () => {
-        canvas.style.width = '100%';
-        canvas.style.height='100%';
-        canvas.width  = canvas.offsetWidth;
-        canvas.height = canvas.offsetHeight;
+    // canvas.style.width = '100%';
+    // canvas.style.height='100%';
+    canvas.style.width = '3000px';
+    canvas.style.height = '3000px';
+    canvas.width = canvas.offsetWidth;
+    canvas.height = canvas.offsetHeight;
 };
 
 const handleMouseMoveOverCanvas = event => {
     event.preventDefault();
     if (!canvasClicked) return;
 
-    const x = event.clientX;
-    const y = event.clientY;
+    const mouseX = parseInt(event.clientX - canvasOffsetX);
+    const mouseY = parseInt(event.clientY - canvasOffsetY);
 
-    canvasParent.scrollLeft = lastX - x;
-    canvasParent.scrollTop = lastY - y;
+    canvas.scrollLeft = mouseX - lastX;
+    canvas.scrollTop = mouseY - lastY;
 
-    lastX = x;
-    lastY = y;
+    lastX = mouseX;
+    lastY = mouseY;
+
+    drawField(field, currentGeneration);
 };
 
 const onCanvasClickRelease = () => {
@@ -40,21 +46,29 @@ const onCanvasClickRelease = () => {
     makeCursor('pointer');
 };
 
-const onCanvasClick = () => {
+const onCanvasClick = (event) => {
     canvasClicked = true;
     makeCursor('grab');
+    lastX = parseInt(event.clientX - canvasOffsetX);
+    lastY = parseInt(event.clientY - canvasOffsetY);
 };
+
+function setCanvasOffset() {
+    canvasOffsetX = canvas.offsetLeft;
+    canvasOffsetY = canvas.offsetTop;
+}
 
 addEventListener('DOMContentLoaded', () => {
     canvasParent = document.querySelector(".game-of-live-canvas");
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
 
+    setCanvasOffset();
     makeCursor('pointer');
     fitCanvasSize();
 
     window.addEventListener('resize', () => fitCanvasSize());
-    canvas.addEventListener('mousedown', () => onCanvasClick());
+    canvas.addEventListener('mousedown', (event) => onCanvasClick(event));
     canvas.addEventListener('mouseup', () => onCanvasClickRelease());
     canvas.addEventListener('mousemove', (event) => handleMouseMoveOverCanvas(event));
 });
@@ -77,9 +91,8 @@ const drawCellOnCanvas = c => {
     ctx.closePath();
 };
 
-const drawCurrentGeneration = () => {
+const drawCurrentGeneration = (currentGeneration) => {
     ctx.beginPath();
-    ++currentGeneration;
     ctx.font = "40px monospace";
     ctx.fillStyle = "red";
     ctx.textAlign = "top";
@@ -87,10 +100,10 @@ const drawCurrentGeneration = () => {
     ctx.closePath();
 };
 
-const drawField = field => {
+const drawField = (field, currentGeneration) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     field.filter(c => c.isAlive).forEach(c => drawCellOnCanvas(c));
-    drawCurrentGeneration();
+    drawCurrentGeneration(currentGeneration);
 };
 
 
@@ -109,11 +122,11 @@ webSocket.onmessage = (event) => {
 
     const rawGameState = event.data;
     const gameState = JSON.parse(rawGameState);
-    const field = gameState.field;
+    field = gameState.field;
     if (!field) return;
 
     console.log(field.length);
-    drawField(field);
+    drawField(field, ++currentGeneration);
 
     webSocket.send(rawGameState);
 
